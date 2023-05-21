a9e4743e0b6b78da549f01ba6f9680d23d71cfd2
Leonid Trofimov <TrofimovL@outlook.com>
2023-05-21 16:33:24 +0300

    Deleted superfluous imports

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {AppComponent} from "../../app.component";
 import {ContactsService} from "../../services/contacts.service";
 
 @Component({
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {ContactsService} from "../../services/contacts.service";
 
 @Component({
